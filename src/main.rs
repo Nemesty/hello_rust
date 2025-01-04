3ad9c6a5b4a932f7ea575879b1dabcb4db4d85cd
Nemesty <frederic.duval@minhbo.fr>
2025-01-04 11:55:35 +0100

    Variables & Constantes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+fn title(titre: &str) {
+    println!("\n⭐ {} :", titre);
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    // Variables
+    {
+        title("Variables");
+        // Variable non modifiable :
+        let x = 5;
+        // Variable modifiable :
+        let mut y = 10;
+        y += 1;
+        // Shadow une variable :
+        let i = "Nemesty";
+        let i = i.len();
+        println!("x = {}\ny = {}\ni = {}", x, y, i);
+    }
+
+    // Constantes
+    {
+        title("Constantes");
+        // Le type doit être indiqué avec les constantes
+        const VALUE_OF_PI: f64 = 3.1415;
+        println!("La valeur de Pi est : {}", VALUE_OF_PI);
+    }
+
+    
 }
