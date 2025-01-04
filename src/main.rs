74850c3c8f4d7f3d91946256c6ca54b7af6beff2
Nemesty <frederic.duval@minhbo.fr>
2025-01-04 12:30:21 +0100

    Array & Tuple

--- src/main.rs
+++ src/main.rs
@@ -26,5 +26,26 @@ fn main() {
         println!("La valeur de Pi est : {}", VALUE_OF_PI);
     }
 
-    
+    // Tuple
+    {
+        title("Tuple");
+        // Les tuples contiennent plusieurs éléments de type différent
+        let perso = (32, "Nemesty", 1.72);
+        // Pattern matching pour déstructurer le tuple
+        let (age, name, height) = perso;
+        println!("Bonjour {}, tu as donc {} ans et tu mesures {}m.",name, age, height);
+        // Il également possible d'accèder à chaque élément par un point
+        let mut age = perso.0;
+        age += 1;
+        println!("Une année est passée vous avez maintenant {} ans.", age);
+    }
+
+    // Array
+    {
+        title("Array");
+        // Les array contiennet plusieurs éléments de même type
+        const DAYS_OF_WEEK: [&str; 7] = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+        println!("Le premier jour de la semaine est le : {}", DAYS_OF_WEEK[0]);
+
+    }
 }
